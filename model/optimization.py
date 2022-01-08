b74f09c4cbd989ec44b8608aaafbeedb92d83713
BounharAbdelaziz <bounhar.abdelaziz@gmail.com>
2022-01-08 14:56:06 +0100

    code refactor

--- model/optimization.py
+++ model/optimization.py
@@ -5,8 +5,19 @@ from torch.nn import init
 from torch.optim import lr_scheduler
 
 import torch.nn.utils as tutils
+from torch.optim import SGD, Adam, AdamW
+import pytorch_warmup
 
-def init_weights(net, init_type='normal', init_gain=0.02):
+
+def define_network(net, data_device, gpu_ids):
+    
+    if len(gpu_ids) > 1:
+        net = torch.nn.DataParallel(net)
+    net.to(data_device)
+   
+    return net
+    
+def init_weights(net, init_type='kaiming', init_gain=0.02):
     """Initialize network weights.
     Parameters:
         net (network)   -- network to be initialized
@@ -43,7 +54,7 @@ def init_weights(net, init_type='normal', init_gain=0.02):
 
     return net
 
-def init_net(net, init_type='normal', init_gain=0.02, gpu_ids=[]):
+def init_net(net, init_type='kaiming', init_gain=0.02, gpu_ids=[]):
     """Initialize a network: 1. register CPU/GPU device (with multi-GPU support); 2. initialize the network weights
     Parameters:
         net (network)      -- the network to be initialized
@@ -59,8 +70,21 @@ def init_net(net, init_type='normal', init_gain=0.02, gpu_ids=[]):
     init_weights(net, init_type, init_gain=init_gain)
     return net 
 
+def get_optimizer(model, options):
 
-def get_scheduler(optimizer, opt):
+    if options.optimizer.upper() == "ADAMW":
+      optimizer = AdamW(model.parameters(), lr=options.lr, betas=(options.beta1, 0.999), weight_decay=options.wd)
+
+    elif options.optimizer.upper() == "ADAM":
+      optimizer = Adam(model.parameters(), betas=(options.beta1, 0.999), lr=options.lr)
+
+    else:
+      optimizer = SGD(model.parameters(), lr=options.lr, weight_decay=options.wd, momentum=0.9)
+
+    return optimizer
+
+    
+def get_scheduler(optimizer, options):
     """Return a learning rate scheduler
     Parameters:
         optimizer          -- the optimizer of the network
@@ -71,26 +95,33 @@ def get_scheduler(optimizer, opt):
     For other schedulers (step, plateau, and cosine), we use the default PyTorch schedulers.
     See https://pytorch.org/docs/stable/optim.html for more details.
     """
-    if opt.lr_policy == 'linear':
+    if options.lr_policy.upper() == 'LINEAR':
         def lambda_rule(epoch):
-            lr_l = 1.0 - max(0, epoch + opt.epoch_count - opt.n_epochs) / float(opt.n_epochs_decay + 1)
+            lr_l = 1.0 - max(0, epoch + options.epoch_count - options.n_epochs) / float(options.n_epochs_decay + 1)
             return lr_l
         scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda_rule)
-    elif opt.lr_policy == 'step':
-        scheduler = lr_scheduler.StepLR(optimizer, step_size=opt.lr_decay_iters, gamma=0.1)
-    elif opt.lr_policy == 'plateau':
+
+    elif options.lr_policy.upper() == 'STEP':
+        scheduler = lr_scheduler.StepLR(optimizer, step_size=options.lr_decay_iters, gamma=0.1)
+
+    elif options.lr_policy.upper() == 'PLATEAU':
         scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.2, threshold=0.01, patience=5)
-    elif opt.lr_policy == 'cosine':
-        scheduler = lr_scheduler.CosineAnnealingLR(optimizer, T_max=opt.n_epochs, eta_min=0)
+
+    elif options.lr_policy.upper() == 'COSINE':
+        scheduler = lr_scheduler.CosineAnnealingLR(optimizer, T_max=options.n_epochs, eta_min=0)
+
+    elif options.lr_policy.upper() == "EXP":
+        scheduler = lr_scheduler.ExponentialLR(optimizer, options.gamma)
+    
+    elif options.lr_policy.upper() == "CYCLICLR":
+        scheduler = lr_scheduler.CyclicLR(optimizer, base_lr=options.lr, max_lr=0.1, step_size_up=5, mode="exp_range", gamma=options.gamma)
+
     else:
-        return NotImplementedError('learning rate policy [%s] is not implemented', opt.lr_policy)
+        return NotImplementedError('learning rate policy [%s] is not implemented', options.lr_policy)
     return scheduler
 
+def get_lr_warmup(optimizer, options):
 
-def define_network(net, data_device, gpu_ids):
-    
-    if len(gpu_ids) > 1:
-        net = torch.nn.DataParallel(net)
-    net.to(data_device)
-   
-    return net
+    return pytorch_warmup.ExponentialWarmup(optimizer, options.warmup_period)
+
+
