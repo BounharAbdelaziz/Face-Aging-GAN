7aeca5100d2c95520552d4ad06ca5f4637d1a928
BounharAbdelaziz <bounhar.abdelaziz@gmail.com>
2022-01-08 14:55:53 +0100

    code refactor + cyclegan tb_writer

--- utils/helpers.py
+++ utils/helpers.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 import os
 import random
+from model.optimization import init_weights, define_network
 
 def write_logs_tb(tb_writer_loss, tb_writer_fake, tb_writer_real, img_fake, img_real, age_fake, losses, step, epoch, hyperparams, with_print_logs=True):
 
@@ -27,10 +28,64 @@ def write_logs_tb(tb_writer_loss, tb_writer_fake, tb_writer_real, img_fake, img_
         print(f"Epoch [{epoch}/{hyperparams.n_epochs}]", sep=' ')
         for k,v in losses.items():
             print(f"{k} : [{v:.7f}]", sep=' - ', end=' - ')
+
+
+def write_logs_tb_cyclegan(tb_writer_loss, tb_writer_fake, tb_writer_real, images_fakes, images_real, losses, step, epoch, hyperparams, with_print_logs=True):
+
+    for k,v in losses.items():
+        tb_writer_loss.add_scalar(
+            'loss/{}'.format(k), v, global_step=step
+        )
+        
+    # Adding generated images to tb
+    tb_writer_fake.add_image(
+        'Fake/domain_1', images_fakes[0], global_step=step
+    )
+
+    tb_writer_real.add_image(
+        "Real/domain_1", images_real[0], global_step=step
+    )
+
+    tb_writer_fake.add_image(
+        'Fake/domain_2', images_fakes[1], global_step=step
+    )
+
+    tb_writer_real.add_image(
+        "Real/domain_2", images_real[1], global_step=step
+    )
+
+    tb_writer_fake.add_image(
+        'Fake/cycle_1', images_fakes[2], global_step=step
+    )
+
+    tb_writer_real.add_image(
+        "Fake/cycle_2", images_fakes[3], global_step=step
+    )
+
+    if with_print_logs :
+        print(f"Epoch [{epoch}/{hyperparams.n_epochs}]", sep=' ')
+        for k,v in losses.items():
+            print(f"{k} : [{v:.7f}]", sep=' - ', end=' - ')
             
     
 def compute_nbr_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+def setup_network(network, hyperparams, type="generator"):
+    print("-----------------------------------------------------")
+    print(f"[INFO] Number of trainable parameters for the {type} : {compute_nbr_parameters(network)}")
+    print("-----------------------------------------------------")
+
+    print(f"[INFO] Initializing the networks...")
+    network=init_weights(network, init_type='kaiming')
+    print("-----------------------------------------------------")
+
+    device_ids = [i for i in range(torch.cuda.device_count())]
+    print(f"[INFO] Setting up {len(device_ids)} GPU(s) for the networks...")
+    print(f"[INFO] .... using GPU(s) device_ids : {device_ids} ...")
+
+    network=define_network(network, hyperparams.device, device_ids)
+    print("-----------------------------------------------------")
 
 def load_model(model, PATH, device='cuda', mode='test'):
     print(f'[INFO] Loading model from {PATH} into device: {device} in mode:{mode}.')
@@ -63,3 +118,7 @@ def create_checkpoints_dir(path):
         print(f'[INFO] Checkpoint directory already exists')
     else:
         print(f'[INFO] Checkpoint directory has been created')
+
+def print_kwargs(**kwargs):
+    for key, value in kwargs.items():
+        print("The value of {} is {}".format(key, value))
