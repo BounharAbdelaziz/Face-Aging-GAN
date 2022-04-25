57ee74b6890fe99f2fcab8db8642c2db01faa95b
BounharAbdelaziz <bounhar.abdelaziz@gmail.com>
2022-04-26 00:25:27 +0200

    changed naming

--- model/base_nets/base_model.py
+++ model/base_nets/base_model.py
@@ -10,8 +10,8 @@ class BaseModel(nn.Module):
         self.experiment = experiment
         # self.hyperparams = hyperparams
 
-        self.tb_writer_fake = SummaryWriter(f"logs/{self.experiment}_GAN/fake_{self.experiment}")
-        self.tb_writer_real = SummaryWriter(f"logs/{self.experiment}_GAN/real_{self.experiment}")
-        self.tb_writer_loss = SummaryWriter(f"logs/{self.experiment}_GAN/loss_train_{self.experiment}")
+        self.tb_writer_fake = SummaryWriter(f"logs/{self.experiment}/fake_{self.experiment}")
+        self.tb_writer_real = SummaryWriter(f"logs/{self.experiment}/real_{self.experiment}")
+        self.tb_writer_loss = SummaryWriter(f"logs/{self.experiment}/loss_train_{self.experiment}")
 
         self.eps = 1e-8
